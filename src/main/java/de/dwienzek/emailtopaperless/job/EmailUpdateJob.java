20241324a2afa5594a7832f17e13619e04f20824
Dominic Wienzek <dominic@dwienzek.de>
2022-09-25 23:33:47 +0200

    Fix duplicate jobs

--- src/main/java/de/dwienzek/emailtopaperless/job/EmailUpdateJob.java
+++ src/main/java/de/dwienzek/emailtopaperless/job/EmailUpdateJob.java
@@ -2,14 +2,15 @@ package de.dwienzek.emailtopaperless.job;
 
 import de.dwienzek.emailtopaperless.component.EmailProcessFunction;
 import de.dwienzek.emailtopaperless.service.EmailFetchService;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import jakarta.mail.MessagingException;
 import java.security.GeneralSecurityException;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
@@ -19,7 +20,7 @@ public class EmailUpdateJob {
     private final EmailFetchService emailFetchService;
     private final EmailProcessFunction emailProcessFunction;
 
-    @Scheduled(fixedDelayString = "${email.update-interval}")
+    @Scheduled(timeUnit = TimeUnit.MINUTES, fixedDelayString = "${email.update-interval}")
     public void fetchEmailsAndConvertToPaperless() {
         LOGGER.info("Processing emails from imap server.");
 
